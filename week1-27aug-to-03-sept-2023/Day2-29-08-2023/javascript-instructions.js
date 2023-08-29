bcf47b931ab471cf72f5dc65637ebb88c9490b63
Masterchinedum <chinedu02k@gmail.com>
2023-08-29 16:16:35 +0100

    basics of javascript

---
+++ week1-27aug-to-03-sept-2023/Day2-29-08-2023/javascript-instructions.js
@@ -0,0 +1,12 @@
+//create a pop up
+
+alert('hello');
+alert('good job!');
+
+//doing mathematics
+2+2 ; // it calculates it an gives the sum of 2+2
+
+//javascript dom
+
+document.body.innerHTML = 'hello'; //this modifies a webpage, in this case it removes everything and replaces it with 'hello'
+
