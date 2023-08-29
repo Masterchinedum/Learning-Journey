ef9c128880ec2b49d40943bde41d6f65429f208b
Masterchinedum <chinedu02k@gmail.com>
2023-08-29 16:38:37 +0100

    lesson 1

--- week1-27aug-to-03-sept-2023/Day2-29-08-2023/javascript-instructions.js
+++ week1-27aug-to-03-sept-2023/Day2-29-08-2023/javascript-instructions.js
@@ -10,3 +10,51 @@ alert('good job!');
 
 document.body.innerHTML = 'hello'; //this modifies a webpage, in this case it removes everything and replaces it with 'hello'
 
+//1a. use alert(...); to display "good morning !" in a popup.
+alert('good morning !');
+
+//1b. Display your name in a popup.
+alert('my name is: CHINEDU');
+
+//1C. Using math, calculate 10 + 5 in ths Console.
+10+5;
+
+//1d. Calculate 20-5 in the console.
+20-5;
+
+//1e. Calculate 10 * 5 in the console.
+10*5;
+//1f. Calculate 2+2-5 in the console
+2+2-5;
+//1g. Use document.body.innerHTML = ...; to display "Good morning!" on the web page
+document.body.innerHTML = 'Good morning!';
+//1h. Use document.body.innerHTML = ...; to display your name on the web page
+document.body.innerHTML = 'CHINEDU';
+
+//1i. You order a T-shirt for $10, socks for $8, and dinner plates for $20 Use javaScript to calculate the total cost of your order
+var Tshirt = 10;
+var socks = 8;
+var dinnerPlates = 20;
+var totalCost = Tshirt + socks + dinnerPlates;
+console.log(totalCost);
+
+//1j. You have $100 in your bank account. You order a T-shirt for $10, socks for $8, and dinner plates for $20. Use javaScript to calculate the amount of money left in your bank account after you order these items.
+var bankAccount = 100;
+var Tshirt = 10;
+var socks = 8;
+var dinnerPlates = 20;
+var totalCost = Tshirt + socks + dinnerPlates;
+var moneyLeft = bankAccount - totalCost;
+console.log(moneyLeft);
+
+//1k. Your bank account has $100, you spend $20 on lunch, $50 on dinner and earn $200 from your job. Calculate how much money you have.
+var bankAccount = 100;
+var lunch = 20;
+var dinner = 50;
+var job = 200;
+var totalCost = lunch + dinner;
+var moneyLeft = bankAccount + job - totalCost;
+console.log(moneyLeft);
+
+//1k. Use document.body.innerHTML = ...; to make the web page blank
+document.body.innerHTML = '';
