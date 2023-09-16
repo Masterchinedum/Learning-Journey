cc970993f2a88336d49a7b14959462dcac64cb62
Masterchinedum <chinedu02k@gmail.com>
2023-09-16 15:32:55 +0100

    add mongo db, code not working

--- week2/Day12/day12 project website/api/index.js
+++ week2/Day12/day12 project website/api/index.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const cors = require('cors') ;
+const express = require ('express');
+const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose'); // Don't forget to import mongoose
 
-app.use(cors())
-
+app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req,res)=>{
-    const {username, password} = req.body;
-    res.json({requestData:{username,password}});
+async function connectToDatabase() {
+  try {
+    await mongoose.connect('mongodb+srv://masterchinedum:vJyxBw5v0aA65zff@cluster0.73akdjd.mongodb.net/?retryWrites=true&w=majority');
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+  }
+}
 
+connectToDatabase(); // Call the function to establish the database connection
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  res.json({ requestData: { username, password } });
 });
 
 app.listen(4000);
+
+//vJyxBw5v0aA65zff
+//mongodb+srv://masterchinedum:vJyxBw5v0aA65zff@cluster0.73akdjd.mongodb.net/?retryWrites=true&w=majority
+
+/* to run backend use 
+nodemon index.js
+*/
+
--- week2/Day12/day12 project website/client/src/post.jsx
+++ week2/Day12/day12 project website/client/src/post.jsx
@@ -3,12 +3,12 @@ export default function Post () {
         
       <div className ="post">
       <div className ="image">
-        <img src = "https://www.lindaikejisblog.com/photos/shares/cover2_1694418923.jpg"/>
+        <img src = "https://www.lindaikejisblog.com/photos/shares/cover2_1694418923.jpg" alt = ""/>
       </div>
       <div className = "texts">
       <h2>Two graduating students of Kwara College to face disciplinary panel</h2>
       <p className = "info">
-        <a className = "author"> Top man</a>
+        <p className = "author"> Top man</p>
         <time>2023-01-06 16:15</time>
       </p>
       <p className = "summary">Two students of the Kwara State College of Education, Ilọrin, have been scheduled to face a disciplinary panel over alleged immoral display during their graduation ceremony. The affected studen</p>
