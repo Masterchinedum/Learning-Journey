b39846a565abd981d8dfd9e3956ce6055b9cf769
Masterchinedum <chinedu02k@gmail.com>
2023-09-16 16:16:05 +0100

    Revert "add more designs to login and register page"
    This reverts commit 38c0c099e39995d151fadc0390f9a48010854824.

--- week2/Day12/day12 project website/client/src/App.js
+++ week2/Day12/day12 project website/client/src/App.js
@@ -1,59 +1,19 @@
-import './App.css'
+import './App.css';
+import Post from "./post";
+import Header from './header';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './layout';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href = "" className = "logo"> Masterchinedum </a>
-        <nav>
-          <a href = "" className = "nav-link">Home</a>
-          <a href = "" className = "nav-link">About</a>
-          <a href = "" className = "nav-link">Contact</a>
-          <a href = "">Login</a>
-          <a href = "">Register</a>
-        </nav>
-      </header>
+    <Routes>
+      <Route path = "/" element = {<Layout />}>
+        <Route index element = {< Post />} />
+        <Route path= {'/login'} element ={<div>login</div>} />
+      </Route>
+    </Routes>
 
-      <div className ="post">
-        <div className ="image">
-          <img src = "https://www.lindaikejisblog.com/photos/shares/cover2_1694418923.jpg"/>
-        </div>
-        <div className = "texts">
-        <h2>Two graduating students of Kwara College to face disciplinary panel</h2>
-        <p className = "info">
-          <a className = "author"> Top man</a>
-          <time>2023-01-06 16:15</time>
-        </p>
-        <p className = "summary">Two students of the Kwara State College of Education, Ilọrin, have been scheduled to face a disciplinary panel over alleged immoral display during their graduation ceremony. The affected studen</p>
-      </div>
-
-        <div className ="image">
-          <img src = "https://www.lindaikejisblog.com/photos/shares/cover2_1694418923.jpg"/>
-        </div>
-        <div className = "texts">
-        <h2>Two graduating students of Kwara College to face disciplinary panel</h2>
-        <p className = "info">
-          <a className = "author"> Top man</a>
-          <time>2023-01-06 16:15</time>
-        </p>
-        <p className = "summary">Two students of the Kwara State College of Education, Ilọrin, have been scheduled to face a disciplinary panel over alleged immoral display during their graduation ceremony. The affected studen</p>
-      </div>
-
-        <div className ="image">
-          <img src = "https://www.lindaikejisblog.com/photos/shares/cover2_1694418923.jpg"/>
-        </div>
-        <div className = "texts">
-        <h2>Two graduating students of Kwara College to face disciplinary panel</h2>
-        <p className = "info">
-          <a className = "author"> Top man</a>
-          <time>2023-01-06 16:15</time>
-        </p>
-        <p className = "summary">Two students of the Kwara State College of Education, Ilọrin, have been scheduled to face a disciplinary panel over alleged immoral display during their graduation ceremony. The affected studen</p>
-      </div>
-
-      </div>
-    </main>
-  )
+  );
 }
 
 export default App;
